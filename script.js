cbf33f2f82f40e9798f0f64b29ee8a23d3d07c32
MindulaHewadewage <mindufernando@gmail.com>
2023-01-18 16:16:57 +0100

    struttura

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log ('JS OK')
+const app = Vue.createApp({
+    name:'Random Mail',
+    data(){
+        return {
+            email:''
+        }
+    },
+})
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(response => {
+
+})
+
+app.mount('#root');
